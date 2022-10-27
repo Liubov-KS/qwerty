4a03e827aea6a5991821529c668dde144b2b586d
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-10-27 16:52:46 +0300

    add Example 001 Hello Console projectI

---
+++ Examples/Example001_HelloConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("122 Hello, World!");
+Console.WriteLine("222 Hello, World!");
+Console.WriteLine("322 Hello, World!");
